092bb547906865d0f9460f451fc8e88e192a2151
Derek Weix <47987393+dpweix@users.noreply.github.com>
2025-09-11 09:09:50 -0400

    Finalize analysis choices for updated data

--- 02_meta_analysis.R
+++ 02_meta_analysis.R
@@ -3,25 +3,12 @@ library(mixmeta)
 
 # Set up meta-regression --------------------------------------------------
 
-# Predictors by city with pre-calculated metadata
-# df_meta <- df_metadata |>
-#   mutate(nsalid1 = nsalid1,
-#          p50 = p50,
-#          temp_range = max - min,
-#          range_high = max - p50,
-#          range_low  = p50 - min,
-#          country = country_num,
-#          KP_level1_Interpretation = KP_level1_Interpretation, 
-#          .keep = "none")
-
-# predictors by city with manually calculated metadata
+# Predictors by city with manually calculated metadata
 df_meta <- df |>
   summarize(p50 = median(tmean),
-            temp_range = max(tmean) - min(tmean),
             range_high = max(tmean) - p50,
             range_low  = min(tmean) - p50,
-            country = first(country), .by = nsalid1) |>
-  left_join(df_metadata, by = join_by(nsalid1 == nsalid1), suffix = c("", "_meta"))
+            country = first(country), .by = nsalid1)
 
 # List of matrices of coefficients (response of meta-regression)
 list_mat_coef <- list_city_pred |> 
@@ -45,11 +32,9 @@ list_model_meta <-
        \(mat_coef, list_vcov) {
          mixmeta(mat_coef ~
                    ns(p50, df = 2) +
-                   #ns(temp_range, df = 2) +
                    ns(range_high, df = 2) +
                    ns(range_low, df = 2) +
                    country,
-                   #KP_level1_Interpretation, 
                  S = list_vcov,
                  data = df_meta)
        })
